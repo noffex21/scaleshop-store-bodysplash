7f22b64c55c23d2abc615c18ca150b58553aff11
Nicolas Pereira <nicolaspereiraxp1@gmail.com>
2026-04-14 14:11:47 -0300

    Deployment ScaleShop Headless - 2026-04-14T17:11:46.724Z

--- src/config/productData.ts
+++ src/config/productData.ts
@@ -24,7 +24,7 @@ export const PRODUCT_DATA = {
   "storeLogo": "https://knbpurdmiwupinkbsxnn.supabase.co/storage/v1/object/public/uploads/1776183139305-1714299153tiktok-shop-logo-transparent.png",
   "storeName": "Feliz Textil Importação",
   "subdomain": "bodysplash",
-  "github_url": "https://github.com/noffex21/scaleshop-store-bodysplash",
+  "vercel_url": "https://scaleshop-bodysplash-0.nl.vercel.app",
   "description": {
     "short": "",
     "specs": [],
@@ -68,9 +68,9 @@ export const PRODUCT_DATA = {
         {
           "id": 1776182923966,
           "text": "Usei pra sair com os amigos e geral perguntou qual era.",
-          "user": "Giovanna R.",
+          "user": "Junior R.",
           "stars": 5,
-          "avatar": "https://p16-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/eab03146b8bfa272b4363cb79083e266~tplv-tiktokx-cropcenter:100:100.jpg?dr=14579&refresh_token=73e7171c&x-expires=1775268000&x-signature=W7AnVLTIamYGkTWGryVf7drZv%2Fs%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=5f8d3399&idc=my2",
+          "avatar": "https://randomuser.me/api/portraits/men/32.jpg",
           "images": [
             "https://knbpurdmiwupinkbsxnn.supabase.co/storage/v1/object/public/uploads/1776182945473-WhatsApp_Image_2026-04-14_at_12.59.10.jpeg"
           ],
@@ -84,7 +84,7 @@ export const PRODUCT_DATA = {
           "text": "Passei antes de sair e uma mulher comentou do nada… já virou meu padrão kkk",
           "user": "Amanda V.",
           "stars": 5,
-          "avatar": "https://p16-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/4d3b68f6305395fde3fe0cb3ad575818~tplv-tiktokx-cropcenter:100:100.jpg?dr=14579&refresh_token=1f90ce04&x-expires=1775268000&x-signature=2mv60IDtJIU50IhUVZjjPKkNihE%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=5f8d3399&idc=my2",
+          "avatar": "https://randomuser.me/api/portraits/men/24.jpg",
           "images": [
             "https://knbpurdmiwupinkbsxnn.supabase.co/storage/v1/object/public/uploads/1776182891732-WhatsApp_Image_2026-04-14_at_12.59.09__1_.jpeg"
           ],
@@ -98,7 +98,7 @@ export const PRODUCT_DATA = {
           "text": "Pelo preço vale muito, melhor que muito perfume caro que já usei.",
           "user": "Fernando P.",
           "stars": 5,
-          "avatar": "https://p16-common-sign.tiktokcdn.com/tos-maliva-avt-0068/b0a24c137a5b9ab3ac40d4c7d6812bfd~tplv-tiktokx-cropcenter:100:100.jpg?dr=14579&refresh_token=96588d84&x-expires=1775268000&x-signature=mOL2LbIXPcy9bAMIqYf4Tuq%2BtHg%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=5f8d3399&idc=my2",
+          "avatar": "https://randomuser.me/api/portraits/men/76.jpg",
           "images": [
             "https://knbpurdmiwupinkbsxnn.supabase.co/storage/v1/object/public/uploads/1776182809753-WhatsApp_Image_2026-04-14_at_12.59.09.jpeg"
           ],
@@ -112,7 +112,7 @@ export const PRODUCT_DATA = {
           "text": "Peguei o preto e o branco, os dois são bons, mas o preto achei mais marcante.",
           "user": "João Pedro",
           "stars": 5,
-          "avatar": "https://p77-sign-va.tiktokcdn.com/tos-maliva-avt-0068/081ed76c7638300fb2a8fb56364c6f40~tplv-tiktokx-cropcenter:100:100.jpg?dr=14579&refresh_token=bc703ed9&x-expires=1775268000&x-signature=C18FqnCXjUxzvY7Pkbwd%2BnMYNmQ%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=5f8d3399&idc=my2",
+          "avatar": "https://randomuser.me/api/portraits/men/93.jpg",
           "images": [
             "https://knbpurdmiwupinkbsxnn.supabase.co/storage/v1/object/public/uploads/1776182743374-WhatsApp_Image_2026-04-14_at_13.05.26.jpeg"
           ],
@@ -126,7 +126,7 @@ export const PRODUCT_DATA = {
           "text": "Chegou rápido e bem embalado. Frasco bonito, produto premium.",
           "user": "Vanessa Lima",
           "stars": 5,
-          "avatar": "https://p16-common-sign.tiktokcdn.com/tos-maliva-avt-0068/7117e093afa695f6b3b91cb7ff54dbdd~tplv-tiktokx-cropcenter:100:100.jpg?dr=14579&refresh_token=83ca2f15&x-expires=1775268000&x-signature=EF1S%2FoX6fPnxU%2B%2B2zto5IJepybg%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=5f8d3399&idc=my2",
+          "avatar": "https://randomuser.me/api/portraits/women/45.jpg",
           "images": [
             "https://knbpurdmiwupinkbsxnn.supabase.co/storage/v1/object/public/uploads/1776182674982-WhatsApp_Image_2026-04-14_at_12.59.08__1_.jpeg"
           ],
@@ -140,7 +140,7 @@ export const PRODUCT_DATA = {
           "text": "Recebi elogio no primeiro dia usando, isso já diz tudo kkkk",
           "user": "Ricardo B.",
           "stars": 5,
-          "avatar": "https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/e78496d97972823bfd30b8bcf9f0cc5a~tplv-tiktokx-cropcenter:100:100.jpg?dr=14579&refresh_token=6e2d2f2c&x-expires=1775268000&x-signature=tFrElwjfvBGIF7WciCoColVTSOc%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=5f8d3399&idc=my2",
+          "avatar": "https://randomuser.me/api/portraits/men/64.jpg",
           "images": [
             "https://knbpurdmiwupinkbsxnn.supabase.co/storage/v1/object/public/uploads/1776182620456-WhatsApp_Image_2026-04-14_at_12.59.08.jpeg"
           ],
@@ -154,7 +154,7 @@ export const PRODUCT_DATA = {
           "text": "Uso depois do banho e antes de sair, dá aquela sensação de limpeza e presença 👌",
           "user": "Juliana F.",
           "stars": 5,
-          "avatar": "https://p16-common-sign.tiktokcdn.com/tos-maliva-avt-0068/b0a24c137a5b9ab3ac40d4c7d6812bfd~tplv-tiktokx-cropcenter:100:100.jpg?dr=14579&refresh_token=96588d84&x-expires=1775268000&x-signature=mOL2LbIXPcy9bAMIqYf4Tuq%2BtHg%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=5f8d3399&idc=my2",
+          "avatar": "https://randomuser.me/api/portraits/women/36.jpg",
           "images": [
             "https://knbpurdmiwupinkbsxnn.supabase.co/storage/v1/object/public/uploads/1776182581867-WhatsApp_Image_2026-04-14_at_12.59.07.jpeg"
           ],
@@ -168,7 +168,7 @@ export const PRODUCT_DATA = {
           "text": "Comprei sem esperar muito e me surpreendi… cheiro de homem arrumado, tipo perfume caro mesmo.",
           "user": "Gabriel H.",
           "stars": 5,
-          "avatar": "https://p16-common-sign.tiktokcdn.com/tos-maliva-avt-0068/f157f599ede20bdc7e97720cb3712d54~tplv-tiktokx-cropcenter:100:100.jpg?dr=14579&refresh_token=92e053b2&x-expires=1775268000&x-signature=7TPsKTndHD3oiWWOXnWNoPdMKh4%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=5f8d3399&idc=my2",
+          "avatar": "https://randomuser.me/api/portraits/men/70.jpg",
           "images": [
             "https://knbpurdmiwupinkbsxnn.supabase.co/storage/v1/object/public/uploads/1776182525573-WhatsApp_Image_2026-04-14_at_12.59.04__1_.jpeg"
           ],
@@ -182,7 +182,7 @@ export const PRODUCT_DATA = {
           "text": "Veio bem embalado e a qualidade surpreendeu positivamente.",
           "user": "João Pedro",
           "stars": 5,
-          "avatar": "https://p77-sign-sg.tiktokcdn.com/tos-alisg-avt-0068/2a5754df6590bd7b8685558bd67a3206~tplv-tiktokx-cropcenter:100:100.jpg?dr=14579&refresh_token=f594ee79&x-expires=1775268000&x-signature=RiK5Dja9sG4i%2F4TKkPzmtGlIDuo%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=5f8d3399&idc=my2",
+          "avatar": "https://randomuser.me/api/portraits/men/40.jpg",
           "images": [
             "https://knbpurdmiwupinkbsxnn.supabase.co/storage/v1/object/public/uploads/1776182402151-WhatsApp_Image_2026-04-14_at_12.59.04.jpeg"
           ],
@@ -196,7 +196,7 @@ export const PRODUCT_DATA = {
           "text": "Muito boa e tem um cheiro bom demais",
           "user": "Henrique S.",
           "stars": 5,
-          "avatar": "https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/e78496d97972823bfd30b8bcf9f0cc5a~tplv-tiktokx-cropcenter:100:100.jpg?dr=14579&refresh_token=6e2d2f2c&x-expires=1775268000&x-signature=tFrElwjfvBGIF7WciCoColVTSOc%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=5f8d3399&idc=my2",
+          "avatar": "https://randomuser.me/api/portraits/men/21.jpg",
           "images": [
             "https://knbpurdmiwupinkbsxnn.supabase.co/storage/v1/object/public/uploads/1776181280058-review-2.jpeg"
           ],
@@ -208,9 +208,9 @@ export const PRODUCT_DATA = {
         {
           "id": 1776181142683,
           "text": "Qualidade do produto: Produto top,super bem embalado minha primeira de muitas compras aqui no tiktok",
-          "user": "Henrique S.",
+          "user": "Sandro S.",
           "stars": 5,
-          "avatar": "https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/4de0150d8d9f2eb736bff99aca81028f~tplv-tiktokx-cropcenter:100:100.jpg?dr=14579&refresh_token=69efb719&x-expires=1775268000&x-signature=tTQo0RgxAMwQhSj0A9oJNQAvQf0%3D&t=4d5b0474&ps=13740610&shp=a5d48078&shcp=5f8d3399&idc=my2",
+          "avatar": "https://randomuser.me/api/portraits/men/8.jpg",
           "images": [
             "https://knbpurdmiwupinkbsxnn.supabase.co/storage/v1/object/public/uploads/1776181232831-review-1.jpeg"
           ],
@@ -280,8 +280,9 @@ export const PRODUCT_DATA = {
   ],
   "carouselImages": [],
   "recommendations": [],
+  "last_deployed_at": "2026-04-14T16:35:48.332Z",
   "paradiseSecretKey": "[REMOVED_FOR_SECURITY]",
-  "github_repo_full_name": "noffex21/scaleshop-store-bodysplash",
+  "vercel_project_id": "prj_aUMnNYyrtEgqhsfNFoINcWBstoAL",
   "isPlaceholder": false
 };
 export default PRODUCT_DATA;
